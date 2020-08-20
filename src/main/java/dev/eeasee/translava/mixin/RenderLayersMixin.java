3b2cde58470d4ad2a48946144fc766d2334896df
X-Green <2271174177@qq.com>
2020-08-20 22:57:15 +0800

    mixin

---
+++ src/main/java/dev/eeasee/translava/mixin/RenderLayersMixin.java
@@ -0,0 +1,17 @@
+package dev.eeasee.translava.mixin;
+
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
+import net.minecraft.fluid.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RenderLayers.class)
+public class RenderLayersMixin {
+    @Inject(method = "getFluidLayer", at = @At("HEAD"), cancellable = true)
+    private static void translucentLava(FluidState state, CallbackInfoReturnable<RenderLayer> cir) {
+        cir.setReturnValue(RenderLayer.getTranslucent());
+    }
+}
